e486b1f37619fb9f27caa85199bbaf7d9144b30d
Lucy <Lucy@BOX-1>
2026-04-03 16:38:59 -0700

    added postgres client pool

--- postgres/client.js
+++ postgres/client.js
@@ -1,1 +1,16 @@
 // Docs: https://node-postgres.com/
+
+import pg from 'pg'
+import 'dotenv/config'
+
+const { Pool } = pg
+
+const client = new Pool({
+  host: 'localhost',
+  port: 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+})
+
+export default client
